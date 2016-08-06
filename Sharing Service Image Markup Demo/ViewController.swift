5d59bb96489775f3284c57a0b5808ed9502288d9
Mark Thalman <mark@ijuggle5.com>
2016-08-06 14:53:43 -0500

    First working version.

--- Sharing Service Image Markup Demo/ViewController.swift
+++ Sharing Service Image Markup Demo/ViewController.swift
@@ -8,20 +8,57 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
-
+class ViewController: NSViewController, NSSharingServiceDelegate
+{
+    @IBOutlet weak var imageView: NSImageView!
+    @IBOutlet weak var filenameLabel: NSTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-    }
-
-    override var representedObject: AnyObject? {
-        didSet {
-        // Update the view, if already loaded.
+        imageView.image = Bundle.main.image(forResource: "DSC_0008.JPG")
+        if let pathname = Bundle.main.path(forResource: "DSC_0008", ofType: "JPG") {
+            filenameLabel.stringValue = pathname
         }
     }
+    
+    override func validateMenuItem(_ menuItem: NSMenuItem) -> Bool {
+        if menuItem.action == #selector(ViewController.markupImage) {
+            return true
+        }
+        return false
+    }
 
+    @IBAction func markupImage(_ sender: AnyObject) {
+        if imageView.image != nil {
+            let service = NSSharingService(named: "com.apple.Preview.Markup")
+            service?.delegate = self
+            let url = NSURL(fileURLWithPath: filenameLabel.stringValue)
+            service?.perform(withItems: [url])
+        }
+    }
+    
+    func sharingService(_ sharingService: NSSharingService, sourceFrameOnScreenForShareItem item: AnyObject) -> NSRect {
+        var rect = NSZeroRect
+        let image = NSImage(byReferencing: item as! URL)
+        rect = NSMakeRect(0, 0, image.size.width, image.size.height)
 
+        return rect
+    }
+    
+    func sharingService(_ sharingService: NSSharingService, sourceWindowForShareItems items: [AnyObject], sharingContentScope: UnsafeMutablePointer<NSSharingContentScope>) -> NSWindow? {
+        return self.view.window
+    }
+
+    func sharingService(_ sharingService: NSSharingService, didShareItems items: [AnyObject]) {
+        let itemProvider : NSItemProvider = items[0] as! NSItemProvider
+    
+        itemProvider.loadItem(forTypeIdentifier: itemProvider.registeredTypeIdentifiers[0] as! String,
+                              options: nil,
+                              completionHandler: {(item, error) -> Void in
+                self.imageView.image = NSImage(byReferencing: item as! URL)
+        })
+    }
 }
 
