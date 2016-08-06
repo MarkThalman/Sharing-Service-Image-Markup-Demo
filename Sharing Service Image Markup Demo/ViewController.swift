5c41d2d21ecd4b467d165957e6212b69255df0aa
Mark Thalman <mark@ijuggle5.com>
2016-08-05 20:48:01 -0500

    Initial Commit

---
+++ Sharing Service Image Markup Demo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Sharing Service Image Markup Demo
+//
+//  Created by Mark Thalman on 8/5/16.
+//  Copyright © 2016 Mark Thalman. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
